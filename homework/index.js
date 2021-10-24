172eaf0947f8dc995cf4b21bed56dca14d142348
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-23 18:37:27 -0700

    updated to work

--- homework/index.js
+++ homework/index.js
@@ -5,6 +5,9 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const makeHTML = require('./util/generateHtml');
 const fs = require('fs');
+const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 //empty array to push employye's into 
 const employeeArr = [];
@@ -47,23 +50,23 @@ function buildEmployees() {
             name: 'startpoint',
             message: 'Start building your team',
             choices: ['Add Engineer', 'Add Intern', 'Finish']
-        }.then(answers => {
-            //witch for multiple inputs scenarios 
-            switch (answers.startpoint) {
-                case 'Add Engineer':
-                    newEngineer();
-                    break;
-                case 'Add Intern':
-                    newIntern();
-                    break;
-                default:
-                  finish();
-                    break
-            }
-        })
+        }
 
+    ]).then(answers => {
+        //witch for multiple inputs scenarios 
+        switch (answers.startpoint) {
+            case 'Add Engineer':
+                newEngineer();
+                break;
+            case 'Add Intern':
+                newIntern();
+                break;
+            default:
+              finish();
+                break
+        }
+    })
 
-    ])
 }
 
 function newEngineer(){
@@ -123,7 +126,13 @@ function newIntern(){
     }
     )
 }
-//Couldn't find right build to populate html file
-// function finish(){
-//     fs.writeFile('./')
-// }
+
+// finish = () =>{
+//     fs.writeFile(outputPath, makeHTML(employeeArr), (err) => {
+//         if (err) throw err;
+//         console.log('The file has been saved!');
+// })}
+
+finish = () =>{
+    fs.writeFileSync(outputPath, makeHTML(employeeArr), "utf-8")}
+
